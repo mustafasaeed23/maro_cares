b7ce0752aa8a2b4f7b50d14e2a36700beba417ae
mustafasaeed23 <mustafasaedd5580@gmail.com>
2024-06-27 07:35:49 +0300

    finished first version of UI

---
+++ lib/presentation/Sliders/Slider.dart
@@ -0,0 +1,56 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:maro/presentation/widgets/trending_products.dart';
+
+class SliderProducts extends StatefulWidget {
+  const SliderProducts({super.key});
+
+  @override
+  State<SliderProducts> createState() => _SliderProductsState();
+}
+
+class _SliderProductsState extends State<SliderProducts> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: 
+        [
+        CarouselSlider(
+          items: [1,2,3,4,5,6].map((e) {
+            return TrendingWidget() ; 
+          }).toList(), 
+          options: CarouselOptions(
+
+            height: 500,
+             aspectRatio: 16/9,
+        viewportFraction: 0.8,
+        initialPage: 0,
+        enableInfiniteScroll: true,
+        reverse: true,
+        autoPlay: true,
+        autoPlayInterval: const Duration(seconds: 3),
+        autoPlayAnimationDuration:const  Duration(milliseconds: 800),
+        autoPlayCurve: Curves.fastOutSlowIn,
+        enlargeCenterPage: true,
+        enlargeStrategy: CenterPageEnlargeStrategy.zoom,
+        enlargeFactor: 0.3,
+        scrollDirection: Axis.horizontal,
+        
+            ),
+            
+           ),
+           DotsIndicator(
+            decorator: const DotsDecorator(
+               color: Colors.black, 
+               
+            ),
+              dotsCount: 8,
+              position: 0
+            )
+        ],
+      ),
+    );
+  }
+}
